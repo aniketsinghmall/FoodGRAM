a88fb5db8dc2b69764161a04738ccb1106f8a1b7
chrisVat <59587035+chrisVat@users.noreply.github.com>
2020-11-24 22:04:29 -0600

    Update popup

--- script.js
+++ script.js
@@ -135,7 +135,7 @@ function goHome(){
 
 function ingredients(restaurant, menuItem){
     windowLocationSearch = window.location.search;
-    document.getElementById('itemChoices').style.display='block';
+    document.getElementById('foodPopup').style.display = "block";
 
     if(document.getElementById("restaurant"+restaurant+"menuItem"+menuItem).innerHTML == ""){
         
